209d79ff83ea48a8e13c731bd9bcc5828489b6c5
yamanmnur <yamanmnur0401@gmail.com>
2021-08-25 23:04:13 +0700

    refactor : constant.java add notes

--- src/Constant.java
+++ src/Constant.java
@@ -1,6 +1,6 @@
 public class Constant {
     /**
-     * BASE PATH YOUR COMPUTER
+     * BASE PATH YOUR COMPUTER FOR CHECK THE FOLDER TEST
      * Sample : D:\\PROJECT\\DropSuiteTest
      */
     public static final String BASEPATH = "D:\\";
